f3eaeb4c922dacb5c3671840b6871e8071757c33
unknown <vidhyasada88@gmail.com>
2021-12-17 18:12:25 -0500

    firsttestsr

---
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -0,0 +1,22 @@
+package com.qa.opencart.pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+	
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.opencart.com/");
+		
+		
+
+	}
+
+}
